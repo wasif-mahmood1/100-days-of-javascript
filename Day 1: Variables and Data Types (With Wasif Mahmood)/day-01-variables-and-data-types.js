ab954bd4f56db9737c27128c7dc3373e80485ab4
wasif-mahmood1 <138897505+wasif-mahmood1@users.noreply.github.com>
2025-03-16 22:56:38 +0500

    Create day-01-variables-and-data-types.js

---
+++ Day 1: Variables and Data Types (With Wasif Mahmood)/day-01-variables-and-data-types.js
@@ -0,0 +1,37 @@
+// Declare variables using let, const, and var
+
+// String variable
+let name = "Wasif Mahmood";
+const greeting = "Hello, Wasif! Welcome to Day 1 of the challenge.";
+
+// Number variable
+let age = 30;
+const year = 2025;
+
+// Boolean variable
+let isStudent = true;
+const isWorking = true;
+
+// Null variable
+let emptyValue = null;
+
+// Undefined variable
+let notDefined;
+
+// Object variable
+let person = {
+    name: "Wasif Mahmood",
+    age: 24,
+    occupation: "Developer"
+};
+
+// Output the values to the console
+console.log("Name:", name);
+console.log("Greeting:", greeting);
+console.log("Age:", age);
+console.log("Year:", year);
+console.log("Is Student:", isStudent);
+console.log("Is Working:", isWorking);
+console.log("Empty Value:", emptyValue);
+console.log("Not Defined:", notDefined);
+console.log("Person Object:", person);
