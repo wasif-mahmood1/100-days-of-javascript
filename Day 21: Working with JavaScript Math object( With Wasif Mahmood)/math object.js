e682e1941d575117d2235207cbcb210a4b5e1199
wasif-mahmood1 <138897505+wasif-mahmood1@users.noreply.github.com>
2025-04-05 23:11:10 +0500

    Create math object.js

---
+++ Day 21: Working with JavaScript Math object( With Wasif Mahmood)/math object.js
@@ -0,0 +1,51 @@
+// Day 21: JavaScript Math Object
+
+// Math Properties (Constants)
+console.log("Math.E:", Math.E); // Euler's number
+console.log("Math.PI:", Math.PI); // PI
+console.log("Math.SQRT2:", Math.SQRT2); // √2
+console.log("Math.SQRT1_2:", Math.SQRT1_2); // √1/2
+console.log("Math.LN2:", Math.LN2); // ln(2)
+console.log("Math.LN10:", Math.LN10); // ln(10)
+console.log("Math.LOG2E:", Math.LOG2E); // log2(e)
+console.log("Math.LOG10E:", Math.LOG10E); // log10(e)
+
+// Rounding Methods
+console.log("Math.round(4.6):", Math.round(4.6));
+console.log("Math.ceil(4.2):", Math.ceil(4.2));
+console.log("Math.floor(4.9):", Math.floor(4.9));
+console.log("Math.trunc(4.7):", Math.trunc(4.7));
+
+// Sign Method
+console.log("Math.sign(-4):", Math.sign(-4));
+console.log("Math.sign(0):", Math.sign(0));
+console.log("Math.sign(4):", Math.sign(4));
+
+// Power and Root
+console.log("Math.pow(8, 2):", Math.pow(8, 2));
+console.log("Math.sqrt(64):", Math.sqrt(64));
+
+// Absolute value
+console.log("Math.abs(-4.7):", Math.abs(-4.7));
+
+// Trigonometry
+console.log("Math.sin(90 * Math.PI / 180):", Math.sin(90 * Math.PI / 180));
+console.log("Math.cos(0 * Math.PI / 180):", Math.cos(0 * Math.PI / 180));
+
+// Min and Max
+console.log("Math.min(0, 150, 30, 20, -8, -200):", Math.min(0, 150, 30, 20, -8, -200));
+console.log("Math.max(0, 150, 30, 20, -8, -200):", Math.max(0, 150, 30, 20, -8, -200));
+
+// Random number
+console.log("Math.random():", Math.random());
+
+// Logarithms
+console.log("Math.log(1):", Math.log(1));
+console.log("Math.log2(8):", Math.log2(8));
+console.log("Math.log10(1000):", Math.log10(1000));
+
+// Summary of Math Methods (demonstration of a few)
+console.log("Math.acos(1):", Math.acos(1));
+console.log("Math.cbrt(27):", Math.cbrt(27));
+console.log("Math.exp(1):", Math.exp(1));
+console.log("Math.tan(Math.PI/4):", Math.tan(Math.PI/4));
